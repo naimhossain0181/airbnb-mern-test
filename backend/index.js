b0ec6c19867878a6adb79eb936a7b54874453f51
Naim Hossain <naimh200@gmail.com>
2023-09-04 13:25:10 +0600

    Update some json

--- backend/index.js
+++ backend/index.js
@@ -27,6 +27,7 @@ app.use("/v1",routes)
 
 
 //server setup
-app.listen(3000,()=>{
-    console.log("server connected port : 3000")
+const port =process.env.PORT||3000
+app.listen(port,()=>{
+    console.log(`Server Connected ${port}`)
 })
